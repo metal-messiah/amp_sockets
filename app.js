83eede7d196947bab9cb05b8b0b63514c4d27ea6
Jordan Porter <jporter@ngacres.com>
2016-10-14 16:57:30 -0600

    add new join emit

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 /**
  * Created by jporter on 10/4/2016.
  */
+
+
+/**
+ * // sending to sender-client only
+ socket.emit('message', "this is a test");
+
+ // sending to all clients, include sender
+ io.emit('message', "this is a test");
+
+ // sending to all clients except sender
+ socket.broadcast.emit('message', "this is a test");
+
+ // sending to all clients in 'game' room(channel) except sender
+ socket.broadcast.to('game').emit('message', 'nice game');
+
+ // sending to all clients in 'game' room(channel), include sender
+ io.in('game').emit('message', 'cool game');
+
+ // sending to sender client, only if they are in 'game' room(channel)
+ socket.to('game').emit('message', 'enjoy the game');
+
+ // sending to all clients in namespace 'myNamespace', include sender
+ io.of('myNamespace').emit('message', 'gg');
+
+ // sending to individual socketid
+ socket.broadcast.to(socketid).emit('message', 'for your eyes only');
+ */
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -28,7 +56,7 @@ io.on('connection', function (socket) {
             username: data.user,
             app: data.app
         }
-        io.sockets.emit("new-join", data)
+        socket.broadcast.emit("new-join", data)
         io.sockets.emit("update-people", users)
     })
 
